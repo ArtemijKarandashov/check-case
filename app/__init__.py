df80ae68cdc264a7423d36531b90dafe64a9bab1
ArtemijKarandashov <artemij.karandashov@yandex.ru>
2025-04-03 12:01:28 +0300

    js_separated

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,9 @@ from flask import Flask
 from flask_socketio import SocketIO
 from app.tools.logger import Logger
 
-app = Flask(__name__)
+import os
+
+app = Flask(__name__,static_folder=os.path.abspath("./static"))
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
---
+++ static/js/socket_handler.js
@@ -0,0 +1,67 @@
+const user_name = document.getElementById('user_name');
+            const session_key = document.getElementById('session_key');
+            const session_data = document.getElementById('session_data');
+            const socket = io();
+            
+            function print_response(data) {
+                const message = document.createElement('p');
+                message.textContent = data.message;
+                session_data.appendChild(message);
+            }
+
+            socket.on('connect', () => {
+                console.log('Connected to WebSocket');
+            });
+
+            socket.on('error', (data) => {
+                console.log('Received:', data);
+                print_response(data)
+            })
+    
+            socket.on('login_success', (data) => {
+                console.log('Received:', data);
+                print_response(data)
+                user_name.innerHTML = `Logined us: ${data.name}`;
+            });
+
+            socket.on('logout_success', (data) => {
+                console.log('You are no longer connected');
+                print_response(data)
+                user_name.innerHTML = `Logined us: My name`;
+            });
+    
+            socket.on('send_session_key', (data) => {
+                console.log('Received:', data);
+                print_response(data)
+                session_key.value = data.session_key;
+            });
+
+            socket.on('user_connected', (data) => {
+                console.log('Received:', data);
+                print_response(data)
+            });
+
+            socket.on('send_result', data => {
+                console.log(data)
+                print_response(data)
+            });
+    
+            function createSession() {
+                socket.emit('create_session');
+            }
+            
+            function joinSession() {
+                socket.emit('join_session', {"session_key": session_key.value});
+            }
+
+            function processCheck(){
+                socket.emit('process_check');
+            }
+            
+            function login(){
+                socket.emit('login', {"name":''})
+            }
+
+            function logout(){
+                socket.emit('logout')
+            }
